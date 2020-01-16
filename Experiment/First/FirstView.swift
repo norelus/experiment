c499fd0a097494b82f20fbf479ac2e82f54b8be0
Aurélien Caille <aurelien.caille@worldline.com>
2020-01-16 13:22:02 +0100

    Début du projet

---
+++ Experiment/First/FirstView.swift
@@ -0,0 +1,21 @@
+//
+//  FirstView.swift
+//  Experiment
+//
+//  Created by occmobility on 16/01/2020.
+//  Copyright © 2020 norelus. All rights reserved.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstView()
+    }
+}
